f9a64c6ebd5bab9d186b6eaaa773f0f3be507104
dilworthc <240337212@aston.ac.uk>
2026-01-29 10:01:45 +0000

    Saved my Lab02 Task03 work

--- lab2-functionalPrimer/haskell/task3.hs
+++ lab2-functionalPrimer/haskell/task3.hs
@@ -1,8 +1,6 @@
-sgn x = if x < 0 
-          then -1 
-          else if x == 0 
-              then 0 
-              else 1
+sgn x | x < 0 = -1 
+      | x == 0 = 0
+      | x > 0 = 1
 
 sgnGuards x | x < 0 = -1
             | x == 0 = 0
@@ -12,4 +10,11 @@ main = do
     putStrLn "Please enter a number"
     input <- getLine 
     let x = (read input :: Int)
-    putStrLn( show (sgn (x)) )
+    putStrLn( show (sgnGuards (x)) )
+
+
+--My Task03 Practice Question answer
+grade score | score >= 90 = "A"
+            | score >= 80 = "B"
+            | score >= 70 = "C"
+            | otherwise = "F"
